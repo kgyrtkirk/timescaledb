81688945d09988e3795c54da3b590c7af7df8658
Zoltan Haindrich <zoltan@timescale.com>
2023-03-02 13:29:15 +0000

    add

--- tsl/test/sql/t1.sql
+++ tsl/test/sql/t1.sql
@@ -1,6 +1,17 @@
 
+select  :'current_mode' = 'normal' as t_normal,
+        :'current_mode' = 'hyper' as t_hyper,
+        :'current_mode' = 'compressed' as t_compressed
+        \gset
+
+-- :RESET_STATE
+
+\set interval_1 '1000 second'
+\set interval_2 '10000 second'
+
+drop schema if exists :current_mode cascade;
 create schema :current_mode;
-set search_path=:current_mode;
+set search_path=:current_mode,public;
 
 CREATE TABLE conditions (
     t timestamp with time ZONE NOT NULL,
@@ -9,11 +20,66 @@ CREATE TABLE conditions (
 
 \if :t_normal
 \else
-    SELECT create_hypertable('conditions', 't', chunk_time_interval => INTERVAL '1 hour');
+    SELECT create_hypertable('conditions', 't', chunk_time_interval => INTERVAL '10 day');
 \endif
+
+INSERT INTO conditions (t, temperature)
+SELECT
+    generate_series('2022-01-01 00:00:00-00'::timestamptz, '2022-01-31 23:59:59-00'::timestamptz, :'interval_1'),
+    0.25;
 
 \if :t_compressed
     ALTER TABLE conditions SET (timescaledb.compress);
+    select compress_chunk(show_chunks('conditions'));
 \endif
 
-:SAVE_STATE
+INSERT INTO conditions (t, temperature)
+SELECT
+    generate_series('2022-01-11 00:00:00-00'::timestamptz, '2022-01-21 23:59:59-00'::timestamptz, :'interval_2'),
+    0.1;
+
+
+INSERT INTO conditions (t, temperature)
+VALUES 
+('2022-01-12 00:00:00-00',1.0),
+('2022-01-12 00:00:00-00',1.0);
+
+
+-- CREATE FUNCTION md5_agg_sfunc(text, anyelement) 
+--        RETURNS text
+--        LANGUAGE sql
+-- AS
+-- $$
+--   SELECT md5($1 || $2::text)
+-- $$;
+-- CREATE  AGGREGATE md5_agg (ORDER BY anyelement)
+-- (
+--   STYPE = text,
+--   SFUNC = md5_agg_sfunc,
+--   INITCOND = ''
+-- );
+
+
+-- \o :current_mode
+-- select * from conditions order by t,temperature;
+-- \o
+
+-- :SAVE_STATE
+
+\if :{?last_mode}
+select :'current_mode' || '<>' || :'last_mode';
+    (
+            select count(1) over (partition by c),* from :current_mode.conditions c
+        except 
+            select count(1) over (partition by c),* from :last_mode.conditions c
+    )
+    union all
+    (
+            select count(1) over (partition by c),* from :last_mode.conditions c
+        except 
+            select count(1) over (partition by c),* from :current_mode.conditions c
+    )
+\endif
+
+\set last_mode :current_mode
+
--- tsl/test/sql/t2.sql
+++ tsl/test/sql/t2.sql
@@ -1,2 +1,11 @@
 
+\unset last_mode
+\set SAVE_STATE
+
+\set current_mode normal
+\ir t1.sql
+-- \set current_mode hyper
+-- \ir t1.sql
+\set current_mode compressed
+\ir t1.sql
 
